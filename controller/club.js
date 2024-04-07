453c84d8bc745c85d1117fe3993372652329ad55
JACTC <97804994+JACTC@users.noreply.github.com>
2024-04-07 17:08:46 +0200

    a

--- controller/club.js
+++ controller/club.js
@@ -391,7 +391,6 @@ const uploadLogo = asyncHandler(async (req, res) => {
     upload.single('logo')(req, res, async (err) => {
         if (err) {
             console.log(err)
-            console.log(req)
             return res.status(415).json({
                 success: false,
                 message: 'Invalid file type. Only images are allowed.',
